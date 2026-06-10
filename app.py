cbecf57e2ca534e9df2489a758e2b2b1ce4be93d
ragnarcasey <ragnarcasey015@gmail.com>
2026-06-10 13:03:02 +0000

    first commit  - flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>App DevOps - Mie Ayam Edition!</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
