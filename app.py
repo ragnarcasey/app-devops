c814fad178fafeec3751571c1370d5789c7567ed
ragnarcasey <ragnarcasey015@gmail.com>
2026-06-13 12:37:57 +0000

    ubah sedikit saja

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>MIE AYAM ITU ENAK BANGETV TAMBAH ES  JERUK</h1>'
+    return '<h1>SAYA HEBAT</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
